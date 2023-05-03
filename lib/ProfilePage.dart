3198ffd312aaa6a73a85eb6ebcb8c53bfc5f1dc7
tladuals <118959195+won0229@users.noreply.github.com>
2023-05-03 12:28:35 +0900

    수정 : 프로필작성, 프로필 페이지

--- lib/ProfilePage.dart
+++ lib/ProfilePage.dart
@@ -1,5 +1,8 @@
 import "package:flutter/material.dart";
-import 'package:meetteam/Appbar3.dart';
+import 'package:meetteam/Appbar2.dart';
+import 'ProfileWritePage.dart';
+import 'package:meetteam/ProfileWritePage.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Profile extends StatelessWidget {
   static const color1 = Color(0xff5dbaf4);
@@ -11,84 +14,91 @@ class Profile extends StatelessWidget {
         appBar: BaseAppBar(key: UniqueKey(), appBar: AppBar()),
         body: Column(children: [
           Container(
-              margin: const EdgeInsets.all(20),
+              margin: const EdgeInsets.all(10),
               child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [Text("프로필")]),
+                    Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [Text("프로필", style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),)],),
                   ])),
           Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                Icon(Icons.account_circle, size: 100),
+                Container(
+                  margin: EdgeInsets.fromLTRB(50, 0, 50, 0),
+                  child: Icon(Icons.account_circle, size: 100),),
+                SizedBox(width : 30.0,),
                 Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: <Widget>[
-                      Text(
-                        "닉네임",
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      Text(
-                        "전문 분야",
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      Text(
-                        "거주 지역",
-                        style: TextStyle(fontSize: 15),
-                      )
+                      Row(children: [
+                        Text("닉네임",textAlign: TextAlign.left, style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+                        SizedBox(width : 20.0,),
+                        Text(ProfileWrite.nicknameController.text),
+                      ],),
+                      SizedBox(height: 20.0,),
+                      Row(children: [
+                        Text("전문분야", textAlign: TextAlign.left, style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+                        SizedBox(width : 20.0,),
+                        Text(ProfileWrite.fieldController.text),
+                      ],),
+                      SizedBox(height: 20.0,),
+                      Row(children: [
+                        Text("거주지역", textAlign: TextAlign.left, style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+                        SizedBox(width : 20.0,),
+                        Text(ProfileWrite.areaController.text),
+                      ],),
                     ])
               ]),
+          SizedBox(height: 40.0,),
           Container(
-            margin: const EdgeInsets.all(20),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text(
-                    "http://www.naver.blog.com",
-                    style: TextStyle(fontSize: 15),
-                  )
-                ]),
+            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Row(children:[
+              Text("블로그", style: TextStyle(fontWeight: FontWeight.bold),),
+              SizedBox(width : 20.0,),
+              TextButton(onPressed: () async {
+                final url = Uri.parse(ProfileWrite.blogController.text);
+                if (await canLaunchUrl(url)){
+                  launchUrl(url);
+                }
+              },
+                child: Text(ProfileWrite.blogController.text),
+              )],
+            ),
           ),
+          SizedBox(height: 20.0,),
           Container(
-            margin: const EdgeInsets.all(20),
-            // 경력
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [Text("경력")]),
+            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [Text("경력", style: TextStyle(fontWeight: FontWeight.bold),)],),
           ),
-          //경력 표시되는 상자
+          SizedBox(height: 10.0,),
           Container(
               margin: const EdgeInsets.all(5),
               padding: const EdgeInsets.all(10),
-              child: Text(
-                  "작열하는 사막에 있는 수상한 남자는 탈취한 보물을 바위 뒤에 숨겨 놓았는데 누군가가 그것을 훔쳐갔다고 화를 내고 있었다.",
-                  style: TextStyle(fontSize: 20)),
+              child: Text(ProfileWrite.careerController.text),
               height: 140,
               width: 450,
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: color1, width: 10))),
+                  border: Border.all(color: color1, width: 5))),
+          SizedBox(height: 10.0,),
           Container(
-            margin: const EdgeInsets.all(20),
+            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
             // 활동내역
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [Text("활동내역")]),
+            child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [Text("활동내역", style: TextStyle(fontWeight: FontWeight.bold),)],),
           ),
+          SizedBox(height: 10.0,),
           //경력 표시되는 상자
           Container(
               margin: const EdgeInsets.all(5),
               padding: const EdgeInsets.all(10),
-              child: Text(
-                  "작열하는 사막에 있는 수상한 남자는 탈취한 보물을 바위 뒤에 숨겨 놓았는데 누군가가 그것을 훔쳐갔다고 화를 내고 있었다.",
-                  style: TextStyle(fontSize: 20)),
+              child: Text(ProfileWrite.activityController.text),
               height: 140,
               width: 450,
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: color1, width: 10))),
+                  border: Border.all(color: color1, width: 5))),
         ]));
   }
 }
