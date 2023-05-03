6cb5c5e5bf0856f8c951db6c0ed297df9611eeac
shushuburger <03robin@naver.com>
2023-05-03 14:37:08 +0900

    추가 : 신청자별 상자 틀 생성

--- lib/UserListPage.dart
+++ lib/UserListPage.dart
@@ -9,6 +9,35 @@ class UserList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: BaseAppBar2(key: UniqueKey(), appBar: AppBar()));
+    return Scaffold(
+        appBar: BaseAppBar2(key: UniqueKey(), appBar: AppBar()),
+        body: Column(children: <Widget>[
+          //신청자 글씨
+          Container(
+              margin: EdgeInsets.only(top: 10, left: 20),
+              width: 1000,
+              child: Text(
+                "신청자",
+                textAlign: TextAlign.left,
+                style: TextStyle(color: color2, fontSize: 23),
+              )),
+
+          //각 신청자별 상자
+          Container(
+            margin: const EdgeInsets.all(5),
+            padding: const EdgeInsets.all(10),
+            height: 150,
+            width: 400,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(color: color3, width: 10)),
+            //상자 내부 구현(이미지, column(row1개(닉네임, 지원 분야),한 줄 소개), 왕관 이미지)
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.account_circle, size: 150),
+              ],
+            ),
+          ),
+        ]));
   }
 }
