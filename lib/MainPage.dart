25df2255ab014f4ab7462fa0b0202449689af8b9
bong-u <bongudev@gmail.com>
2023-05-09 13:39:17 +0900

    추가 : carousel_slider

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:meetteam/ProjectWritePage.dart';
 import 'package:meetteam/Appbar/MainAppbar.dart';
 import 'package:meetteam/ProjectWritePage.dart';
 import 'package:meetteam/ProjectListPage.dart';
 import 'package:meetteam/Color.dart';
+import 'package:meetteam/Widgets/ProjectCard.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -96,90 +98,52 @@ class MainPage extends StatelessWidget {
                   ],
                 ),
                 // 추천 프로젝트 리스트
-                Stack(children: [
-                  Container(
-                      height: 360,
-                      margin: const EdgeInsets.fromLTRB(0, 10, 50, 10),
-                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                      decoration: BoxDecoration(
-                          color: CustomColor.color3,
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: <Widget>[
-                                  const ClipOval(
-                                    child: Icon(
-                                      Icons.person,
-                                      size: 50,
-                                    ),
-                                  ),
-                                  const Text(
-                                    "Nickname",
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18,
-                                        color: Colors.white),
-                                  ),
-                                  Container(
-                                      height: 40,
-                                      width: 100,
-                                      margin: const EdgeInsets.only(top: 5),
-                                      decoration: BoxDecoration(
-                                          color: CupertinoColors.systemGrey3,
-                                          borderRadius:
-                                              BorderRadius.circular(37)),
-                                      child: const Text("D-2",
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 30,
-                                              color: Colors.white),
-                                          textAlign: TextAlign.center)),
-                                ]),
-                            Container(
-                              height: 180,
-                              margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                              decoration: const BoxDecoration(
-                                  color: CupertinoColors.systemGrey3),
-                            ),
-                            const Text(
-                              "  프로젝트명",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  color: Colors.white),
-                            ),
-                            const Text(
-                              "   한줄 소개",
-                              style:
-                                  TextStyle(fontSize: 13, color: Colors.white),
-                            )
-                          ])),
-                  Positioned(
-                    bottom: 0,
-                    right: 20,
-                    child: MaterialButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ProjectWritePage(),
-                            ));
-                      },
-                      color: CupertinoColors.systemGrey3,
-                      textColor: Colors.black,
-                      padding: const EdgeInsets.all(20),
-                      shape: const CircleBorder(),
-                      child: const Icon(
-                        Icons.create,
-                        size: 20,
+                CarouselSlider(
+                    items: [
+                      ProjectCard(
+                        title: "프로젝트1",
+                        description: "설명1",
+                        nickname: "user1",
+                        dDay: "3",
                       ),
+                      ProjectCard(
+                        title: "프로젝트2",
+                        description: "설명2",
+                        nickname: "user2",
+                        dDay: "6",
+                      ),
+                    ],
+                    options: CarouselOptions(
+                      height: 380.0,
+                      enlargeCenterPage: false,
+                      autoPlay: true,
+                      aspectRatio: 16 / 9,
+                      autoPlayCurve: Curves.fastOutSlowIn,
+                      enableInfiniteScroll: true,
+                      autoPlayAnimationDuration: Duration(milliseconds: 800),
+                      viewportFraction: 1,
+                    )),
+                Positioned(
+                  bottom: 0,
+                  right: 20,
+                  child: MaterialButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProjectWritePage(),
+                          ));
+                    },
+                    color: CupertinoColors.systemGrey3,
+                    textColor: Colors.black,
+                    padding: const EdgeInsets.all(20),
+                    shape: const CircleBorder(),
+                    child: const Icon(
+                      Icons.create,
+                      size: 20,
                     ),
-                  )
-                ])
+                  ),
+                )
               ]))
         ]));
   }
