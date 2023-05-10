32046ceaa34e5c66c94a017c98d8d2c37cd29b8c
pphy03 <djbsjse22@naver.com>
2023-05-10 15:43:14 +0900

    삭제 : 기존 플로팅 버튼 제거

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -111,90 +111,67 @@ class MainPage extends StatelessWidget {
                   ],
                 ),
                 // 추천 프로젝트 리스트
-                Stack(children: [
-                  Container(
-                      height: 360,
-                      margin: const EdgeInsets.fromLTRB(0, 10, 50, 10),
-                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                      decoration: BoxDecoration(
-                          color: CustomColor.color3,
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: <Widget>[
-                                  const ClipOval(
-                                    child: Icon(
-                                      Icons.person,
-                                      size: 50,
-                                    ),
+                Container(
+                    height: 360,
+                    margin: const EdgeInsets.fromLTRB(0, 10, 50, 10),
+                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    decoration: BoxDecoration(
+                        color: CustomColor.color3,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
+                              mainAxisAlignment:
+                              MainAxisAlignment.spaceAround,
+                              children: <Widget>[
+                                const ClipOval(
+                                  child: Icon(
+                                    Icons.person,
+                                    size: 50,
                                   ),
-                                  const Text(
-                                    "Nickname",
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18,
-                                        color: Colors.white),
-                                  ),
-                                  Container(
-                                      height: 40,
-                                      width: 100,
-                                      margin: const EdgeInsets.only(top: 5),
-                                      decoration: BoxDecoration(
-                                          color: CupertinoColors.systemGrey3,
-                                          borderRadius:
-                                              BorderRadius.circular(37)),
-                                      child: const Text("D-2",
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 30,
-                                              color: Colors.white),
-                                          textAlign: TextAlign.center)),
-                                ]),
-                            Container(
-                              height: 180,
-                              margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                              decoration: const BoxDecoration(
-                                  color: CupertinoColors.systemGrey3),
-                            ),
-                            const Text(
-                              "  프로젝트명",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  color: Colors.white),
-                            ),
-                            const Text(
-                              "   한줄 소개",
-                              style:
-                                  TextStyle(fontSize: 13, color: Colors.white),
-                            )
-                          ])),
-                  Positioned(
-                    bottom: 0,
-                    right: 20,
-                    child: MaterialButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ProjectWritePage(),
-                            ));
-                      },
-                      color: CupertinoColors.systemGrey3,
-                      textColor: Colors.black,
-                      padding: const EdgeInsets.all(20),
-                      shape: const CircleBorder(),
-                      child: const Icon(
-                        Icons.create,
-                        size: 20,
-                      ),
-                    ),
-                  )
-                ])
+                                ),
+                                const Text(
+                                  "Nickname",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                      color: Colors.white),
+                                ),
+                                Container(
+                                    height: 40,
+                                    width: 100,
+                                    margin: const EdgeInsets.only(top: 5),
+                                    decoration: BoxDecoration(
+                                        color: CupertinoColors.systemGrey3,
+                                        borderRadius:
+                                        BorderRadius.circular(37)),
+                                    child: const Text("D-2",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 30,
+                                            color: Colors.white),
+                                        textAlign: TextAlign.center)),
+                              ]),
+                          Container(
+                            height: 180,
+                            margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                            decoration: const BoxDecoration(
+                                color: CupertinoColors.systemGrey3),
+                          ),
+                          const Text(
+                            "  프로젝트명",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                color: Colors.white),
+                          ),
+                          const Text(
+                            "   한줄 소개",
+                            style:
+                            TextStyle(fontSize: 13, color: Colors.white),
+                          )
+                        ])),
               ]))
         ]));
   }
