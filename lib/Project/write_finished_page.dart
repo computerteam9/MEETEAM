ca073eb80f70b12386910eea792db8f67145dd91
bong-u <bongudev@gmail.com>
2023-05-14 14:48:11 +0900

    개선 : write_finished_page로 통일

--- lib/Project/write_finished_page.dart
+++ lib/Project/write_finished_page.dart
@@ -1,35 +1,42 @@
 import "package:flutter/material.dart";
 import "package:meetteam/Appbar/normal_appbar.dart";
-import "package:meetteam/application_check_page.dart";
 import "package:meetteam/Color.dart";
+import "project_read_page.dart";
 
-class ApplicationFinishedPage extends StatefulWidget {
-  const ApplicationFinishedPage({super.key});
+class WriteFinishedPageArgument {
+  final String? label;
 
-  @override
-  State<StatefulWidget> createState() => ApplicationFinishedPageState();
-
+  WriteFinishedPageArgument(this.label);
 }
 
-class ApplicationFinishedPageState extends State<ApplicationFinishedPage> {
+class WriteFinishedPage extends StatefulWidget {
+  const WriteFinishedPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => WriteFinishedPageState();
+}
+
+class WriteFinishedPageState extends State<WriteFinishedPage> {
   @override
   Widget build(BuildContext context) {
+    final args =
+        ModalRoute.of(context)!.settings.arguments as WriteFinishedPageArgument;
     return Scaffold(
       appBar: BaseAppbar(key: UniqueKey(), appBar: AppBar()),
       bottomNavigationBar: SizedBox(
         child: GestureDetector(
           onTap: () {
             Navigator.push(
-                context, MaterialPageRoute(
-              builder: (context) => const ApplicationCheckPage(),
-            ));
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ProjectReadPage(),
+                ));
           },
           child: Container(
             height: 60,
             color: CustomColor.color3,
             alignment: Alignment.center,
-            child:
-            const Text(
+            child: const Text(
               "확인",
               style: TextStyle(
                 color: Colors.white,
@@ -39,18 +46,12 @@ class ApplicationFinishedPageState extends State<ApplicationFinishedPage> {
           ),
         ),
       ),
-      body: const Center(
-        child:
-        Text(
-          "지원이 완료되었습니다.",
-          style: TextStyle(
-              fontSize: 20
-          ),
+      body: Center(
+        child: Text(
+          args.label!,
+          style: TextStyle(fontSize: 20),
         ),
       ),
     );
-    // TODO: implement build
-    throw UnimplementedError();
   }
-
 }
