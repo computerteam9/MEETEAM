620cec86f991d06e924af72c36a8ba8d043292b8
bong-u <bongudev@gmail.com>
2023-05-14 23:51:45 +0900

    추가 : 만남 방식, 시간

--- lib/Project/project_write_page.dart
+++ lib/Project/project_write_page.dart
@@ -15,12 +15,8 @@ class ProjectWritePage extends StatefulWidget {
 
   static String projectTitle =
       _ProjectWritePageState().projectTitleController.text;
-  static String requiredCareer =
-      _ProjectWritePageState().requiredCareerController.text;
   static String workingTime =
       _ProjectWritePageState().workingTimeController.text;
-  static String personNumber =
-      _ProjectWritePageState().personNumberController.text;
   static String introduceProject =
       _ProjectWritePageState().introduceProjectController.text;
 
@@ -36,6 +32,7 @@ class _ProjectWritePageState extends State<ProjectWritePage> {
   TextEditingController introduceProjectController = TextEditingController();
 
   static const iconColor = Colors.black;
+  static const meetingWay = ["만남 방식", "온라인", "오프라인"];
   static const applicantLabel = [
     ["분야 선택", "개발", "디자인", "기획", "기타"],
     ["경력 선택", "신입", "경력", "무관"],
@@ -44,19 +41,21 @@ class _ProjectWritePageState extends State<ProjectWritePage> {
 
   List imageList = [];
   List fileList = [];
-  List applicantInfo = [
+  List selectedApplicantInfo = [
     [
       "분야 선택",
       "경력 선택",
       "기술 선택",
     ]
   ];
+  String selectedMeetingWay = meetingWay[0];
+  String meetingTime = "";
 
   void addApplicantInputField() {
     // 최대 5명까지 추가 가능
-    if (applicantInfo.length < 5) {
+    if (selectedApplicantInfo.length < 5) {
       setState(() {
-        applicantInfo.add([
+        selectedApplicantInfo.add([
           "분야 선택",
           "경력 선택",
           "기술 선택",
@@ -67,9 +66,9 @@ class _ProjectWritePageState extends State<ProjectWritePage> {
 
   void removeApplicantInputField() {
     // 최소 1명까지 삭제 가능
-    if (applicantInfo.length > 1) {
+    if (selectedApplicantInfo.length > 1) {
       setState(() {
-        applicantInfo.removeLast();
+        selectedApplicantInfo.removeLast();
       });
     }
   }
@@ -80,49 +79,47 @@ class _ProjectWritePageState extends State<ProjectWritePage> {
         backgroundColor: Colors.white,
         appBar: BaseAppbar(key: UniqueKey(), appBar: AppBar()),
         body: SingleChildScrollView(
-          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-            Container(
-              margin: const EdgeInsets.fromLTRB(30, 30, 30, 0),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: "프로젝트 제목",
-                      ),
-                      textAlign: TextAlign.start,
-                      controller: projectTitleController,
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: "경력",
-                      ),
-                      textAlign: TextAlign.start,
-                      controller: requiredCareerController,
-                      minLines: 1,
-                      maxLines: 3,
-                    ),
-                    // ),
-                  ]),
+            child: Container(
+          margin: const EdgeInsets.fromLTRB(30, 30, 30, 0),
+          child:
+              Wrap(runSpacing: 10, alignment: WrapAlignment.center, children: [
+            Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: "프로젝트 제목",
+                ),
+                textAlign: TextAlign.start,
+                controller: projectTitleController,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              // ),
+            ]),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: "프로젝트 내용",
+              ),
+              textAlign: TextAlign.start,
+              controller: introduceProjectController,
+              minLines: 1,
+              maxLines: 3,
             ),
             Column(
               children: [
                 // 지원자 수만큼 반복
-                for (int i = 0; i < applicantInfo.length; i++)
+                for (int i = 0; i < selectedApplicantInfo.length; i++)
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       // 지원자 정보 입력 (분야, 경력, 기술)
                       for (int j = 0; j < applicantLabel.length; j++)
                         DropdownButton<String>(
-                          value: applicantInfo[i][j],
+                          value: selectedApplicantInfo[i][j],
                           onChanged: (value) {
                             setState(() {
-                              applicantInfo[i][j] = value!;
+                              selectedApplicantInfo[i][j] = value!;
                             });
                           },
                           items: [
@@ -138,8 +135,7 @@ class _ProjectWritePageState extends State<ProjectWritePage> {
                   ),
               ],
             ),
-            Container(
-                child: Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 TextButton(
@@ -149,19 +145,36 @@ class _ProjectWritePageState extends State<ProjectWritePage> {
                     onPressed: () => addApplicantInputField(),
                     child: Text("인원 추가"))
               ],
-            )),
-            Container(
-              margin: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "프로젝트 내용",
-                ),
-                textAlign: TextAlign.start,
-                controller: introduceProjectController,
-                minLines: 1,
-                maxLines: 3,
-              ),
             ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              DropdownButton<String>(
+                value: selectedMeetingWay,
+                onChanged: (value) {
+                  setState(() {
+                    selectedMeetingWay = value!;
+                  });
+                },
+                items: [
+                  for (int i = 0; i < meetingWay.length; i++)
+                    DropdownMenuItem<String>(
+                      value: meetingWay[i],
+                      child: Text(meetingWay[i]),
+                    ),
+                ],
+              ),
+              Expanded(
+                child: TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      meetingTime = value;
+                    });
+                  },
+                  decoration: const InputDecoration(
+                    hintText: '만남 시간',
+                  ),
+                ),
+              ),
+            ]),
             Container(
                 //여기에 업로드된 파일 리스트
                 child: (() {
@@ -179,7 +192,7 @@ class _ProjectWritePageState extends State<ProjectWritePage> {
                 //여기에 업로드된 파일 리스트
                 ),
             Container(
-              margin: const EdgeInsets.all(30),
+              // margin: const EdgeInsets.all(30),
               alignment: Alignment.centerLeft,
               child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
                 IconButton(
@@ -204,6 +217,6 @@ class _ProjectWritePageState extends State<ProjectWritePage> {
               },
             ),
           ]),
-        ));
+        )));
   }
 }
