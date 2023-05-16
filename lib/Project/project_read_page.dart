6f7a76bd93b2b5805d0b237085d37ed60168cdf1
tlrznf46 <tlrznf46@gmail.com>
2023-05-16 23:14:35 +0900

    수정 : project_read_page

--- lib/Project/project_read_page.dart
+++ lib/Project/project_read_page.dart
@@ -58,31 +58,39 @@ class _ProjectReadPage extends State<ProjectReadPage> {
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Container(
-              margin: EdgeInsets.fromLTRB(0, 0, 0, 30),
-              alignment: Alignment.topLeft,
-              child: Text(
-                "프로젝트명",
-                style: TextStyle(
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
-                ),
-              )
-            ),
-              Container(
+                margin: EdgeInsets.fromLTRB(0, 0, 0, 30),
                 alignment: Alignment.topLeft,
-                child: const Text(
-                  "프로젝트 설명",
+                child: Text(
+                  "프로젝트명",
                   style: TextStyle(
-                    fontSize: 20,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+            Container(
+              alignment: Alignment.topLeft,
+              child: const Text(
+                "프로젝트 설명",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Text(
+              //프로젝트 설명
+              ProjectWritePage.introduceProject ,
+              maxLines: 10,
+              overflow: TextOverflow.ellipsis,
+            ),
+
+                Container(
+                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 10),
+                  alignment: Alignment.topLeft,
+                  child: const Text(
+                    "리더",
+                    style: TextStyle(fontSize: 20),
                   ),
                 ),
-              ),
-              Text(
-                //프로젝트 설명
-                ProjectWritePage.introduceProject,
-                maxLines: 10,
-                overflow: TextOverflow.ellipsis,
-              ),
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
@@ -99,7 +107,7 @@ class _ProjectReadPage extends State<ProjectReadPage> {
                     height: 50,
                     width: 100,
                     alignment: Alignment.topLeft,
-                    child: Text("닉네임")),
+                    child: Text("닉네임")), // 닉네임
                 Container(
                   margin: EdgeInsets.fromLTRB(100, 30, 0, 0),
                   height: 40,
@@ -108,214 +116,170 @@ class _ProjectReadPage extends State<ProjectReadPage> {
                       color: CustomColor.color3,
                       borderRadius: BorderRadius.all(Radius.circular(10))),
                   alignment: Alignment.center,
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: CustomColor.color3, // Background color
+                  child: const Text(
+                    "프로필",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              alignment: Alignment.topLeft,
+              child: const Text(
+                "모임",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(100, 10, 0, 0),
+              height: 30,
+              alignment: Alignment.topLeft,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: const [
+                      Text("오프라인  "),
+                      Text(
+                        "zoom",
+                        style: TextStyle(color: Colors.blueAccent),
                       ),
-                      child: Text("프로필", style: TextStyle(color: Colors.white)),
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/profile');
-                      })
-                ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(0, 20, 0, 10),
-                alignment: Alignment.topLeft,
-                child: const Text(
-                  "리더",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                    height: 50,
-                    width: 50,
-                    alignment: Alignment.centerLeft,
-                    decoration: const BoxDecoration(
-                        color: Colors.grey, shape: BoxShape.circle),
+                    ],
                   ),
-                  Container(
-                      margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
-                      height: 50,
-                      width: 100,
-                      alignment: Alignment.topLeft,
-                      child: Text("닉네임")), // 닉네임
-                  Container(
-                    margin: EdgeInsets.fromLTRB(100, 30, 0, 0),
-                    height: 40,
+                  Text(ProjectWritePage.workingTime) //기간
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              child: const Text(
+                "모집 인원",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  alignment: Alignment.centerLeft,
+                  decoration: const BoxDecoration(
+                      color: Colors.grey, shape: BoxShape.circle),
+                ),
+                Container(
+                    margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                    height: 50,
                     width: 80,
-                    decoration: const BoxDecoration(
-                        color: CustomColor.color3,
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                    alignment: Alignment.center,
-                    child: const Text(
-                      "프로필",
-                      style: TextStyle(color: Colors.white),
-                    ),
+                    alignment: Alignment.topLeft,
+                    child: Text("백엔드")),
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                  height: 60,
+                  width: 200,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                          child: Column(
+                        children: [
+                          Text("최소 경력"),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(0, 2, 0, 0),
+                            height: 20,
+                            width: 50,
+                            alignment: Alignment.center,
+                            decoration: const BoxDecoration(
+                                color: CustomColor.color1,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10))),
+                            child: const Text(
+                              "3년",
+                              style: TextStyle(color: CustomColor.color3),
+                            ),
+                          )
+                        ],
+                      )),
+                      Container(
+                          child: Column(
+                        children: [
+                          const Text("필요 기술"),
+                          Container(
+                            margin: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                            height: 30,
+                            width: 30,
+                            decoration: const BoxDecoration(
+                                color: Colors.grey, shape: BoxShape.circle),
+                          )
+                        ],
+                      )),
+                    ],
                   ),
-                ],
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                alignment: Alignment.topLeft,
-                child: const Text(
-                  "모임",
-                  style: TextStyle(fontSize: 20),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(100, 10, 0, 0),
-                height: 30,
-                alignment: Alignment.topLeft,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Row(
-                      children: const [
-                        Text("오프라인  "),
-                        Text(
-                          "zoom",
-                          style: TextStyle(color: Colors.blueAccent),
-                        ),
-                      ],
-                    ),
-                    Text(ProjectWritePage.workingTime) //기간
-                  ],
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  alignment: Alignment.centerLeft,
+                  decoration: const BoxDecoration(
+                      color: Colors.grey, shape: BoxShape.circle),
                 ),
-              ),
-              Container(
-                alignment: Alignment.topLeft,
-                child: const Text(
-                  "모집 인원",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
+                Container(
+                    margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
                     height: 50,
-                    width: 50,
-                    alignment: Alignment.centerLeft,
-                    decoration: const BoxDecoration(
-                        color: Colors.grey, shape: BoxShape.circle),
+                    width: 80,
+                    alignment: Alignment.topLeft,
+                    child: Text("프론트엔드")),
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                  height: 60,
+                  width: 200,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                          child: Column(
+                        children: [
+                          Text("최소 경력"),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                            height: 20,
+                            width: 50,
+                            alignment: Alignment.center,
+                            decoration: const BoxDecoration(
+                                color: CustomColor.color1,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10))),
+                            child: const Text(
+                              "5년",
+                              style: TextStyle(color: CustomColor.color3),
+                            ),
+                          )
+                        ],
+                      )),
+                      Column(
+                        children: [
+                          const Text("필요 기술"),
+                          Container(
+                            margin: const EdgeInsets.fromLTRB(0, 2, 0, 0),
+                            height: 30,
+                            width: 30,
+                            decoration: const BoxDecoration(
+                                color: Colors.grey, shape: BoxShape.circle),
+                          )
+                        ],
+                      ),
+                    ],
                   ),
-                  Container(
-                      margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
-                      height: 50,
-                      width: 80,
-                      alignment: Alignment.topLeft,
-                      child: Text("백엔드")),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
-                    height: 60,
-                    width: 200,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Container(
-                            child: Column(
-                          children: [
-                            Text("최소 경력"),
-                            Container(
-                              margin: EdgeInsets.fromLTRB(0, 2, 0, 0),
-                              height: 20,
-                              width: 50,
-                              alignment: Alignment.center,
-                              decoration: const BoxDecoration(
-                                  color: CustomColor.color1,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10))),
-                              child: const Text(
-                                "3년",
-                                style: TextStyle(color: CustomColor.color3),
-                              ),
-                            )
-                          ],
-                        )),
-                        Container(
-                            child: Column(
-                          children: [
-                            const Text("필요 기술"),
-                            Container(
-                              margin: const EdgeInsets.fromLTRB(0, 5, 0, 0),
-                              height: 30,
-                              width: 30,
-                              decoration: const BoxDecoration(
-                                  color: Colors.grey, shape: BoxShape.circle),
-                            )
-                          ],
-                        )),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    height: 50,
-                    width: 50,
-                    alignment: Alignment.centerLeft,
-                    decoration: const BoxDecoration(
-                        color: Colors.grey, shape: BoxShape.circle),
-                  ),
-                  Container(
-                      margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
-                      height: 50,
-                      width: 80,
-                      alignment: Alignment.topLeft,
-                      child: Text("프론트엔드")),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
-                    height: 60,
-                    width: 200,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Container(
-                            child: Column(
-                          children: [
-                            Text("최소 경력"),
-                            Container(
-                              margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
-                              height: 20,
-                              width: 50,
-                              alignment: Alignment.center,
-                              decoration: const BoxDecoration(
-                                  color: CustomColor.color1,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10))),
-                              child: const Text(
-                                "5년",
-                                style: TextStyle(color: CustomColor.color3),
-                              ),
-                            )
-                          ],
-                        )),
-                        Column(
-                          children: [
-                            const Text("필요 기술"),
-                            Container(
-                              margin: const EdgeInsets.fromLTRB(0, 2, 0, 0),
-                              height: 30,
-                              width: 30,
-                              decoration: const BoxDecoration(
-                                  color: Colors.grey, shape: BoxShape.circle),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ]
+                ),
+              ],
+            ),
+]
         ),
       ),
     );
