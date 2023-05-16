261c3836e9aeb1f965a44d76e174d533a09b981e
bong-u <bongudev@gmail.com>
2023-05-16 22:26:54 +0900

    수정 : import 문제

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:meetteam/notice_page.dart';
-import 'package:meetteam/application_check_page.dart';
 import 'package:meetteam/profile/signup_page.dart';
 import 'package:meetteam/main_page.dart';
 import 'package:meetteam/profile/profile_page.dart';
@@ -10,7 +9,6 @@ import 'package:meetteam/Project/project_write_page.dart';
 import 'package:meetteam/project/project_read_page.dart';
 import 'package:meetteam/user_list_page.dart';
 import 'Project/user_check_page.dart';
-import 'application_read_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,22 +20,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(),
-      initialRoute: '/signup',
-      routes: {
-        '/signup': (context) => SignUpPage(),
-        '/': (context) => MainPage(),
-        '/projects': (context) => ProjectListPage(),
-        '/projectWrite': (context) => ProjectWritePage(),
-        '/projectRead': (context) => ApplicationReadPage(),
-        '/project': (context) => ProjectReadPage(),
-        '/profile': (context) => ProfilePage(),
-        '/explorer': (context) => ExplorerPage(),
-        '/userList': (context) => UserList(),
-        '/userCheck' : (context) => UserCheckPage(),
-        '/notice' : (context) => Notice_page(),
-      });
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(),
+        initialRoute: '/signup',
+        routes: {
+          '/signup': (context) => SignUpPage(),
+          '/': (context) => MainPage(),
+          '/projects': (context) => ProjectListPage(),
+          '/projectWrite': (context) => ProjectWritePage(),
+          '/project': (context) => ProjectReadPage(),
+          '/profile': (context) => ProfilePage(),
+          '/explorer': (context) => ExplorerPage(),
+          '/userList': (context) => UserList(),
+          '/userCheck': (context) => UserCheckPage(),
+          '/notice': (context) => Notice_page(),
+        });
   }
 }
