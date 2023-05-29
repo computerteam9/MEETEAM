25e47c94cef0092fdb708281cc9ab3b5617e6a72
pphy03 <djbsjse22@naver.com>
2023-05-29 21:50:17 +0900

    추가 : 로그인 페이지에 로고 추가

--- lib/Profile/login_page.dart
+++ lib/Profile/login_page.dart
@@ -11,6 +11,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
@@ -38,62 +39,89 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(children: [
-          SizedBox(
-            height: 100,
-            width: 200,
-            child:
-            Row(children: [
-              Column(
-                children: <Widget>[
-                  Row(children: [
-                    Text("Id : "),
-                    TextFormField(
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(50, 150, 50, 10),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget> [
+                SizedBox(
+                  child: Image.asset('assets/images/meeteam_logo.png'),
+                ),
+                SizedBox(
+                  height: 60,
+                ),
+                Row(
+                  mainAxisAlignment:MainAxisAlignment.spaceAround,
+                  children : [
+                  SizedBox(
+                    height: 40.0,
+                    width: 300,
+                    child: TextFormField(
                       controller: _emailController,
                       onChanged: (value) {
                         _CompleteCondition();
                       },
+                      decoration: InputDecoration(
+                          hintText: '이메일',
+                      ),
                     ),
-                  ]),
-                  Row(children: [
-                    Text("Password : "),
-                    TextFormField(
+                    ),
+                ],),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment:MainAxisAlignment.spaceAround,
+                  children: [
+                  SizedBox(
+                    height: 40.0,
+                    width: 300,
+                    child: TextFormField(
                       controller: _passwordController,
                       onChanged: (value) {
                         _CompleteCondition();
                       },
-                      decoration: InputDecoration(),
+                      decoration: InputDecoration(
+                        hintText: '비밀번호',
+                      ),
                     ),
-                  ]),
-                ],
-              ),
-              ElevatedButton(
-                child: Text(
-                  "로그인",
-                  style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ],),
+                SizedBox(
+                  height: 35.0,
                 ),
-                onPressed: () {
-                  if (_CompleteLogin) {
-                    Session.set(UserApi.verifyUser(
-                        _emailController.text, _passwordController.text)
-                        .toString());
-                    Navigator.pushNamed(context, '/');
-                  }
-                },
-              )
-            ]),
-          ),
-
-          TextButton(
-              onPressed: () => Navigator.pushNamed(context, '/signup'),
-              child: Text(
-                "회원 가입",
-              )
+                SizedBox(
+                  height: 45,
+                  width: 300,
+                  child: ElevatedButton(
+                    child: Text(
+                      "로그인",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    onPressed: () {
+                      if (_CompleteLogin) {
+                        Session.set(UserApi.verifyUser(
+                            _emailController.text, _passwordController.text)
+                            .toString());
+                        Navigator.pushNamed(context, '/');
+                      }
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                TextButton(
+                    onPressed: () => Navigator.pushNamed(context, '/signup'),
+                    child: Text(
+                      "아직 회원가입을 하지 않으셨나요?",
+                    )
+                )
+              ],
+            ),
           )
-        ]),
-      ),
+        )
     );
-    // TODO: implement build
   }
 }
