553960ce9aea7e7a3a848ed7f59a2f3ca12041ef
tlrznf46 <tlrznf46@gmail.com>
2023-05-29 19:28:52 +0900

    추가 : login_page 로그인 버튼 추가 및 기능 구현

--- lib/Profile/login_page.dart
+++ lib/Profile/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:meetteam/Api/session.dart';
+import 'package:meetteam/Api/user_api.dart';
+import 'package:meetteam/Color.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -9,42 +11,77 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _idController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+
+  bool _CompleteLogin = false;
+
+  void _CompleteCondition() {
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    String userId = UserApi.verifyUser(email, password).toString();
+
+    if (_emailController.text.isNotEmpty &&
+        _passwordController.text.isNotEmpty &&
+        userId != '') {
+      setState(() {
+        _CompleteLogin = true;
+      });
+    } else {
+      setState(() {
+        _CompleteLogin = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Row(
-                  children: [
-                    Text(
-                        "Id : "
-                    ),
-                    TextFormField(
-                      controller: _idController,
-                      decoration: InputDecoration(
-                      ),
-                    ),
-                  ]),
-              Row(
-                  children: [
-                    Text(
-                        "Password : "
-                    ),
-                    TextFormField(
-                      controller: _passwordController,
-                      decoration: InputDecoration(
-                      ),
-                    ),
-                  ]),
-            ],
-          )
+        child: Column(children: [
+          Row(children: [
+            Column(
+              children: <Widget>[
+                Row(children: [
+                  Text("Id : "),
+                  TextFormField(
+                    controller: _emailController,
+                    onChanged: (value) {
+                      _CompleteCondition();
+                    },
+                  ),
+                ]),
+                Row(children: [
+                  Text("Password : "),
+                  TextFormField(
+                    controller: _passwordController,
+                    onChanged: (value) {
+                      _CompleteCondition();
+                    },
+                    decoration: InputDecoration(),
+                  ),
+                ]),
+              ],
+            ),
+            ElevatedButton(
+              child: Text(
+                "로그인",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () {
+                if (_CompleteLogin) {
+                  Session.set(UserApi.verifyUser(
+                          _emailController.text, _passwordController.text)
+                      .toString());
+                  Navigator.pushNamed(context, '/');
+                }
+              },
+            )
+          ]),
+        ]),
       ),
     );
     // TODO: implement build
   }
-
 }
