9dad2d2e77ef3d6a15ebddf189de44224276d7d3
bong-u <bongudev@gmail.com>
2023-05-29 17:29:38 +0900

    추가 : updateUser 함수

--- lib/Api/user_api.dart
+++ lib/Api/user_api.dart
@@ -75,4 +75,47 @@ class UserApi {
       }
     });
   }
+
+  static Future<void> updateUser(
+      String id,
+      String email,
+      String password,
+      String nickname,
+      String introduction,
+      String blogUrl,
+      List<Map<String, int>> spec,
+      List<String> interest) async {
+    User newUser =
+        User(email, password, nickname, introduction, blogUrl, spec, interest);
+
+    if (nickname != newUser.nickname) {
+      await DB.instance
+          .collection('users')
+          .where('nickname', isEqualTo: nickname)
+          .get()
+          .then((queryResult) {
+        if (queryResult.docs.isNotEmpty)
+          throw Exception('해당 닉네임으로 이미 가입되었습니다.');
+      });
+    }
+    if (email != newUser.email) {
+      await DB.instance
+          .collection('users')
+          .where('email', isEqualTo: email)
+          .get()
+          .then((queryResult) {
+        if (queryResult.docs.isNotEmpty) throw Exception('해당 이메일로 이미 가입되었습니다.');
+      });
+    }
+
+    await DB.instance.collection('users').doc(id).update({
+      'email': newUser.email,
+      'password': newUser.password,
+      'nickname': newUser.nickname,
+      'introduction': newUser.introduction,
+      'blogUrl': newUser.blogUrl,
+      'spec': newUser.spec,
+      'interest': newUser.interest,
+    });
+  }
 }
