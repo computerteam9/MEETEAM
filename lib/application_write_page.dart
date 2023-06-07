5c8663ccb79e6c93e64d7f1b3dbf75bfcfc6a424
tlrznf46 <tlrznf46@gmail.com>
2023-06-07 19:53:52 +0900

    삭제 : 한 줄 소개 빼고 삭제

--- lib/application_write_page.dart
+++ lib/application_write_page.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:meetteam/Appbar/write_appbar.dart';
-import "package:meetteam/Api/session.dart";
-import "package:meetteam/Api/user_api.dart";
 
 // workingTime(신청자 가능한 시간) 변수 사용을 안함
 
 class ApplicationWritePage extends StatefulWidget {
   ApplicationWritePage({super.key});
 
-  static String nickName = _ApplicationWritePageState().nickNameController.text;
-  static String supportArea =
-      _ApplicationWritePageState().supportAreaController.text;
-  static String myCareer = _ApplicationWritePageState().myCareerController.text;
-  static String workingTime =
-      _ApplicationWritePageState().workingTimeController.text;
   static String introduceUser =
       _ApplicationWritePageState().introduceUserController.text;
 
@@ -22,19 +14,9 @@ class ApplicationWritePage extends StatefulWidget {
 }
 
 class _ApplicationWritePageState extends State<ApplicationWritePage> {
-  TextEditingController nickNameController = TextEditingController();
-  TextEditingController supportAreaController = TextEditingController();
-  TextEditingController myCareerController = TextEditingController();
-  TextEditingController workingTimeController = TextEditingController();
   TextEditingController introduceUserController = TextEditingController();
 
   static const iconColor = Colors.black;
-
-  @override
-  void dispose() {
-    introduceUserController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,77 +25,16 @@ class _ApplicationWritePageState extends State<ApplicationWritePage> {
       appBar: BaseAppbar(key: UniqueKey(), appBar: AppBar()),
       body: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
         Container(
-          margin: const EdgeInsets.fromLTRB(30, 30, 30, 0),
-          child:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-            Flexible(
-              fit: FlexFit.tight,
-              flex: 7,
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: "닉네임",
-                ),
-                textAlign: TextAlign.start,
-                controller: nickNameController,
-              ),
-            ),
-            Spacer(flex: 1),
-            Flexible(
-              fit: FlexFit.tight,
-              flex: 10,
-              child: TextFormField(
-                decoration: const InputDecoration(labelText: "지원 분야"),
-                textAlign: TextAlign.start,
-                controller: supportAreaController,
-                minLines: 1,
-                maxLines: 3,
-              ),
-            ),
-          ]),
-        ),
-        Container(
-          margin: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: "경력",
-              ),
-              textAlign: TextAlign.start,
-              controller: myCareerController,
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: "활동 시간"),
-              textAlign: TextAlign.start,
-              controller: workingTimeController,
-              minLines: 1,
-              maxLines: 3,
-            ),
-          ]),
-        ),
-        Container(
-          margin: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+          margin: const EdgeInsets.fromLTRB(50, 0, 30, 0),
           child: TextFormField(
             decoration: const InputDecoration(
               labelText: "한 줄 소개",
             ),
             textAlign: TextAlign.start,
             controller: introduceUserController,
-            minLines: 1,
-            maxLines: 3,
+            minLines: 2,
+            maxLines: 5,
           ),
-        ),
-        Container(
-          margin: const EdgeInsets.all(30),
-          alignment: Alignment.centerLeft,
-          child: const Icon(
-            Icons.add,
-            color: iconColor,
-            size: 30,
-          ),
-          // const IconButton(
-          //   icon: Icon(Icons.add, color:iconColor, size: 30),
-          //   onPressed: () => {print("hi")},
-          // ),
         ),
       ]),
     );
